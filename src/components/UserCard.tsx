e37fc929c046796256af9f09267bacec9b7fd62d
Jenbert Marzano <jenbertmarzano@gmail.com>
2024-10-28 17:52:41 +0800

    update

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -9,8 +9,8 @@ const UserCard = ({ type }: { type: string }) => {
         </span>
         <Image src="/more.png" alt="" width={20} height={20} />
       </div>
-      <h1>1,234</h1>
-      <h2>{type}</h2>
+      <h1 className="text-2xl font-semibold my-4">1,234</h1>
+      <h2 className="capitalize text-sm font-medium text-gray-500">{type}</h2>
     </div>
   );
 };
