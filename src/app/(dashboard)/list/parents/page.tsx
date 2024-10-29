5e9fa83a4a670f9f899a413c30fa492690d0feba
Jenbert Marzano <jenbertmarzano@gmail.com>
2024-10-29 18:00:27 +0800

    update

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -1,20 +1,16 @@
-import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { role, teachersData } from "@/lib/data";
+import { role, parentsData } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
-type Teacher = {
+type Parent = {
   id: number;
-  teacherId: string;
   name: string;
   email?: string;
-  photo: string;
+  students: string[];
   phone: string;
-  subjects: string[];
-  classes: string[];
   address: string;
 };
 
@@ -25,20 +21,8 @@ const columns = [
   },
 
   {
-    header: "Teacher ID",
-    accessor: "teacherId",
-    className: "hidden md:table-cell",
-  },
-
-  {
-    header: "Subjects",
-    accessor: "subjects",
-    className: "hidden md:table-cell",
-  },
-
-  {
-    header: "Classes",
-    accessor: "classes",
+    header: "Student Names",
+    accessor: "students",
     className: "hidden md:table-cell",
   },
 
@@ -60,42 +44,32 @@ const columns = [
   },
 ];
 
-const TeacherListPage = () => {
-  const renderRow = (item: Teacher) => (
+const ParentListPage = () => {
+  const renderRow = (item: Parent) => (
     <tr
       key={item.id}
       className="border-b border-gray even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
     >
       <td className="flex items-center gap-4 p-4">
-        <Image
-          src={item.photo}
-          alt=""
-          width={40}
-          height={40}
-          className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
-        />
         <div className="flex flex-col">
           <h3 className="font-semibold">{item.name}</h3>
           <p className="text-xs text-gray-500">{item?.email}</p>
         </div>
       </td>
-      <td className="hidden md:table-cell">{item.teacherId}</td>
-      <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
-      <td className="hidden md:table-cell">{item.classes.join(",")}</td>
+      <td className="hidden md:table-cell">{item.students.join(",")}</td>
       <td className="hidden md:table-cell">{item.phone}</td>
       <td className="hidden md:table-cell">{item.address}</td>
       <td>
         <div className="flex items-center gap-2">
           <Link href={`/list/teachers/${item.id}`}>
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-              <Image src="/view.png" alt="" width={16} height={16} />
+              <Image src="/edit.png" alt="" width={16} height={16} />
             </button>
           </Link>
           {role === "admin" && (
-            // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-            //   <Image src="/delete.png" alt="" width={16} height={16} />
-            // </button>
-            <FormModal table="teacher" type="delete" id={item.id} />
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+              <Image src="/delete.png" alt="" width={16} height={16} />
+            </button>
           )}
         </div>
       </td>
@@ -106,7 +80,7 @@ const TeacherListPage = () => {
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* top */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Parents</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -117,20 +91,19 @@ const TeacherListPage = () => {
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
-              // <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-              //   <Image src="/plus.png" alt="" width={14} height={14} />
-              // </button>
-              <FormModal table="teacher" type="create" />
+              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                <Image src="/plus.png" alt="" width={14} height={14} />
+              </button>
             )}
           </div>
         </div>
       </div>
       {/* list */}
-      <Table columns={columns} renderRow={renderRow} data={teachersData} />
+      <Table columns={columns} renderRow={renderRow} data={parentsData} />
       {/* pagination */}
       <Pagination />
     </div>
   );
 };
 
-export default TeacherListPage;
+export default ParentListPage;
--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -3,20 +3,22 @@ import BigCalendar from "@/components/BigCalendar";
 import EventCalendar from "@/components/EventCalendar";
 
 const StudentPage = () => {
-  return <div className="p-4 flex gap-4 flex-col xl:flex-row">
-    {/* left */}
-    <div className="w-full xl:w-2/3">
-      <div className="h-full bg-white p-4 rounded-md">
-        <h1 className="text-xl font-semibold">Schedule (4A)</h1>
-        <BigCalendar/>
+  return (
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* left */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* right */}
+      <div className="w-full xl:w-1/3 flex=col gap-8">
+        <EventCalendar />
+        <Announcements />
       </div>
     </div>
-    {/* right */}
-    <div className="w-full xl:w-1/3 flex=col gap-8">
-      <EventCalendar/>
-      <Announcements/>
-      </div>
-  </div>
+  );
 };
 
 export default StudentPage;
